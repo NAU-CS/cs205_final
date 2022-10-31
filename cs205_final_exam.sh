50373922f73da69610b4e46150f3b7636f79e091
dc2638 <116678140+dc2638@users.noreply.github.com>
2022-10-31 13:25:37 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,18 @@
+#!/bin/awk -f
+
+#this first part just takes the entered file name and places it here
+BEGIN{print "File name: " ARGV[1]}
+
+#prints the end of the list with the total of all pokemon
+END{print "Total Pokemon: " FNR}
+
+#calculates the mean of pokemon HP
+{Total+= $6} {HPmean= Total/FNR} END{print "Avg. HP: " HPmean}
+
+#calculates the mean of pokemon attacks
+{attackTotal+= $7} {attackMean= attackTotal/FNR} END{print "Avg. Attack: " attackMean} 
+
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
