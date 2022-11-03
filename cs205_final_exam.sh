80024d5c72b7aa391f8f715fa850cc0694c6e993
Ethan-w4b <83723953+Ethan-w4b@users.noreply.github.com>
2022-11-03 12:49:46 -0700

    Update cs205_final_exam.sh
    first commit

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,12 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+#!/bin/bash
+file=$1
+count=0;
+total=0; 
+
+# extract the desired columns from the file
+cut --compliment -d' ' -f6,7,8 file > new_pokemon.dat
+#print out the average of hp, attack, and defense
+awk -F, '{for(i=1; i<=NF; i++) {a[i]+=$i; if($i!="") b[i]++}}; END {for(i=1; i<=NF; i++) printf "%s%s", a[i]/b[i], (i==NF?ORS:OFS)}' new_pokemon.dat
