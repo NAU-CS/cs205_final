142bd297068e88b3e6832e495f3b4e449612a724
thetravisweber <tbroweber@gmail.com>
2022-11-03 20:36:27 -0700

    made awk script to summarize pokemon data, and screenshot of run

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/awk -f
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
@@ -10,3 +12,23 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN {
+    filename = ARGV[1]
+}
+
+# Do inbetween beginning and end
+{
+    if (NR > 1) totalHealth += $6
+
+    if (NR > 1) totalAttack += $7
+}
+
+END {
+    print("===== SUMMARY OF DATA FILE =====")
+    print("   File name: ", filename)
+    print("   Total Pokemon: ", NR -1)
+    print("   Avg. Attack: ", totalAttack / (NR -1))
+    print("   Avg. HP: ", totalHealth / (NR -1))
+    print("===== END SUMMARY =====")
+}
