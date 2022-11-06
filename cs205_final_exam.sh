6d8b8688a3d4b4b3f37455ca2afa8fd98507ea20
FinnBoyds <116925467+FinnBoyds@users.noreply.github.com>
2022-11-05 20:38:42 -0700

    added script

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,25 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#! /usr/bin/awk -f
+BEGIN {
+#save name of of the target file
+filename = ARGV[1]
+}
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+{
+# total hp of all targets
+if (NR > 1) HPsum += $6
+
+# total atk of all targets
+if (NR > 1) ATKsum += $7
+
+# total number of targets in target document
+numTar = NR -1
+}
+
+END {
+ print "===== SUMMARY OF DATA FILE ====="
+ print "   File name: ", filename
+ print "   Total Pokemon: ", numPokemon
+ print "   Avg. Attack: ", ATKsum / numTar
+ print "   Avg. HP: ", HPsum / numTar
+ print "===== END SUMMARY ====="
+}
