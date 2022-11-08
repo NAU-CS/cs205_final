994dc2ba37072c393d97d8d35db3a0892dd50829
swm64 <swm64@nau.edu>
2022-11-07 22:54:06 -0700

    Updated cs205_final_exam.sh to display file name, total pokemon, and average HP and Attack. Included screenshot folder of working scripts.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,25 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+# Assign Variables
+var=totalPokemon
+var=aveHP
+var=aveAtk
+
+# Get number of lines in file, assign to totalPokemon
+totalPokemon=$(wc -l < $1)
+# One line is the header, so decrease value by 1.
+totalPokemon=$((totalPokemon-1))
+
+# Average up all values in column 5 for average HP
+aveHP=$(awk '{total+= $5 } END { print total/NR}' $1)
+
+# Average all values in column 6 for average attack
+aveAtk=$(awk '{total+= $6 } END { print total/NR}' $1)
+
+echo "===== SUMMARY OF DATA FILE ====="
+echo "    File name: $1"
+echo "    Total Pokemon: $totalPokemon"
+echo "    Avg. HP: $aveHP"
+echo "    Avg. Attack: $aveAtk"
