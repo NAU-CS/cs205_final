544598bb8ca55df6b18143705ee2927a73bece1b
Brayden <bjr345@nau.edu>
2022-11-08 20:48:36 -0700

    finalchanges

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -6,6 +6,12 @@
 #    Avg. Attack: [VALUE]
 # ===== END SUMMARY =====
 
+awk -f 'BEGIN{FS="\t"}' pokemon.dat #Set tab as field separator
+awk 'END{print "File name: " FILENAME }' pokemon.dat #Print filename
+awk 'END{print "Total Pokemon: " (NR - 1)}' pokemon.dat #print total pokemon minus one because of title/first line
+awk 'END{ print "Avg. HP: " ((sum+=$6)/(NR-1))}' pokemon.dat #print avg HP 
+awk 'END{ print "Avg. Attack: " ((sum+=$7)/(NR-1))}' pokemon.dat #print avg attack
+
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
