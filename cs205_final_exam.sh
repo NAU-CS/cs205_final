c94da3e4359e72f339f214e8855701da9bd0600b
Justin Stouffer <jbs368@nau.edu>
2022-11-11 23:05:43 -0700

    Modified script to correctly print the desired output, included screenshot of output in screenshots folder

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,24 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#This line prints the words SUMMARY OF DATA FILE to screen
+echo ===== SUMMARY OF DATA FILE =====
+
+#This line prints the first argument to the screen
+echo    File name: $1
+
+#This line prints out the number of lines -1 to the screen, which in this case is the number of pokemon in the file 
+awk 'END{print "Total Pokemon: "NR-1}' $1
+
+#This line calculates the Average HP by summing up all the values in column 6, and then dividing it by the number of lines -1
+awk 'BEGIN{FS="\t"; sum=0} {sum+=$6} 
+END{print "Avg. HP: "sum / (NR-1)}' $1
+
+#This line calculates the average attack of each pokemon using similar methods
+awk 'BEGIN{FS="\t"; sum=0} {sum+=$7}
+END{print "Avg. Attack: "sum/(NR-1)}' $1
+
+#This line prints the words END SUMMARY to screen
+echo ===== END SUMMARY =====
+
