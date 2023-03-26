d4c5b65bbc1597112eabe38efe59c2a7ed9e8462
cmd644 <cmd644@nau.edu>
2023-03-26 13:54:14 -0700

    Modified final exam shell script
    +- cs205_final_exam.sh
    [Filled out the TODO section. Let the shell script pass in a .dat file similar to the pokemon.dat, and made sure that the file did the correct processing.]
    ++ screenshots\terminalOutput.png
    [Proof of processing.]

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,4 +1,4 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# DONE: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
@@ -8,5 +8,12 @@
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
+# There should be a comment explaining the purpose of each line in your shell script.
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+FILE="$1"
+cat $FILE
+if [[ -e "$FILE" ]]; then
+    awk 'BEGIN{FS="\t"} {sumHP+=$6} {sumAttack+=$7} END{printf "File name: %s\n Total Pokemon: %d\n Avg. HP: %d\n Avg. Attack: %d\n", FILENAME, NR, sumHP/NR, sumAttack/NR}' $FILE
+else
+    echo "$(pwd)/$FILE does not exist!"
+fi
