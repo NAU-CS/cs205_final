1f4cfa3b32dc0599c5282308a04af629d8d0bded
cmd644 <cmd644@nau.edu>
2023-03-26 14:06:27 -0700

    Minor modifications to script for readability
    +- cs205_final_exam.sh
    [Modify the script for readability]
    +- terminalOutput.png
    [Modify the picture to show simplified output]

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,10 +10,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script.
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+# saves the positional argument to a FILE variable
 FILE="$1"
-cat $FILE
+# checks to see if the file exists.
 if [[ -e "$FILE" ]]; then
+    # if the file exists, then use awk to process the file.
     awk 'BEGIN{FS="\t"} {sumHP+=$6} {sumAttack+=$7} END{printf "File name: %s\n Total Pokemon: %d\n Avg. HP: %d\n Avg. Attack: %d\n", FILENAME, NR, sumHP/NR, sumAttack/NR}' $FILE
 else
+    # if the file DNE, make sure to tell the user that the file doesn't exist!
     echo "$(pwd)/$FILE does not exist!"
 fi
+# end
