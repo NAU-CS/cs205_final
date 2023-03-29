44628791dfaf3455e2288458443b66c20d12b844
lily quintanar <lilyquintanar@lilys-MacBook-Air-5.local>
2023-03-29 11:58:58 -0700

    added script to cs205_final_exam.sh added screenshot of terminal when ./cs205_final_exam.sh pokemon.dat is run

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,4 +1,4 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+#TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
@@ -10,3 +10,19 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+#command to print header
+echo "==== SUMMARY OF DATA FILE ===="
+#sets positional parameter as variable to print file name
+FILE=$1
+#command to print file name
+echo "	File name: $FILE"
+#command to calculate and print total pokemon
+cat  $FILE | awk '{sum +=$5 } END {print "	Total Pokemon: " sum}'
+#command to calculate and print average HP
+cat $FILE | awk '{sum+=$6} END { print "	Avg. HP: ",sum/NR}'
+#command to calculate adn print average attack
+cat $FILE | awk '{sum+=$7} END { print "	Avg. Attack: ",sum/NR}'
+#command to print last line of text
+echo "==== END SUMMARY ===="
