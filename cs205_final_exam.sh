706267325f85e24b8e107b9bfd7f70eb4a6df078
Michael <mikejcasta@gmail.com>
2023-03-29 16:44:17 -0700

    Ran script for pokemon.dat and took screenshot as instructed.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,25 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+
+#!/bin/bash
+
+# Set variables to hold the column indices
+NAME_COL=2
+HP_COL=5
+ATTACK_COL=6
+
+# Use awk to calculate the values
+FILENAME=$1
+TOTAL=$(awk 'FNR > 1 {count++} END{print count}' $FILENAME) # count the number of lines (pokemon) after the first line
+AVG_HP=$(awk 'FNR > 1 {total += $HP_COL} END{print total/(NR-1)}' HP_COL=$HP_COL $FILENAME) # sum HP column (excluding first line) and calculate mean
+AVG_ATTACK=$(awk 'FNR > 1 {total += $ATTACK_COL} END{print total/(NR-1)}' ATTACK_COL=$ATTACK_COL $FILENAME) # sum ATTACK column (excluding first line) and calculate mean
+
+# Print the summary
+echo "# ===== SUMMARY OF DATA FILE ====="
+echo "#    File name: $FILENAME"
+echo "#    Total Pokemon: $TOTAL"
+echo "#    Avg. HP: $AVG_HP"
+echo "#    Avg. Attack: $AVG_ATTACK"
+echo "# ===== END SUMMARY ====="
