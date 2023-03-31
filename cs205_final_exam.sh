3221da2e8c2b2c2b25960bce27f7174491b6bada
Ryan Wood <rmw367@nau.edu>
2023-03-31 10:26:32 -0700

    Modify cs205_final_exam.sh and add screenshot of terminal output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,25 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+#!/bin/bash
+
+# This script reads a file formatted like pokemon.dat and provides a summary report.
+
+# Store the filename in a variable
+filename=$1
+
+# Calculate the total number of Pokemon
+total_pokemon=$(awk 'END {print NR}' $filename)
+
+# Calculate the average HP
+avg_hp=$(awk '{hp_total+=$2} END {print hp_total/NR}' $filename)
+
+# Calculate the average Attack
+avg_attack=$(awk '{attack_total+=$3} END {print attack_total/NR}' $filename)
+
+# Print the summary report
+echo "# ======= SUMMARY OF $filename ======"
+echo "#    Total Pokemon: $total_pokemon"
+echo "#    Avg. HP: $avg_hp"
+echo "#    Avg. Attack: $avg_attack"
+echo "# ======= END SUMMARY ======="
