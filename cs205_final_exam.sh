539ca6a6dc11ea68212e42b05fb2ed0cb3f069c9
jr3649 <jr3649@nau.edu>
2023-03-31 16:39:25 -0700

    all of the modifications made make it so you are shown the name of the file, the amount of pokemon in the file, their average health, and attack

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,18 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#!/bin/bash
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+file="pokemon.dat"						# assigns whats in the .dat to "file" 
+total=$(awk 'END{print NR}' $file)				# this gets the total # of pokemon and saves it to the variable total
+avg_hp=$(awk '{sum +=$6} END{print sum/NR}' $file) 		# this is supposed to get the average hp of the pokemon which is around 69, I tried using the code 
+								# "(awk 'BEGIN{FS='\t'} {sum+=$6 if (NR!=1) {hp+=1} } END{print sum/NR}' $file)" which was provided by the 
+								# professor after HW 1 but I kept getting a syntax error 
+								# pokemon as per the code provided by 
+avg_attack=$(awk '{sum += $7} END{print sum/NR}' $file)		# this gets the average attack of the 	
+								# pokemon
+
+echo "===== SUMMARY OF DATA FILE ====="				# the rest of these lines put the answer 
+echo "   File name: $file"					# into the requred template by passing 
+echo "   Total Pokemon: $total"					# the vaiables which have the hopefully 
+echo "   Avg. HP: $avg_hp"					# correct number in them using the echo
+echo "   Avg. Attack: $avg_attack"				# command.
+echo "===== END SUMMARY ====="					#
+
