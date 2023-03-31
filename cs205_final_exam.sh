de91c3bb9672645decf1a9129c75f2d04ef30553
Creeking <iraffino13@gmail.com>
2023-03-31 13:23:01 -0700

    adding final sh and screenshots.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,4 +1,16 @@
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+
+file=$1
+total=$(awk 'END {print NR-1}' $file)
+hp=$(awk '{sum += $6} END {print sum / $total}' $file)
+attack=$(awk '{sum += $7} END {print sum / $total}' $file)
+
+echo "SUMMARY OF DATA FILE ($file):"
+echo "- Total Pokemon: $total"
+echo "- Avg. HP: $hp"
+echo "- Avg. Attack: $attack"
+
+
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
@@ -8,5 +20,5 @@
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
+# There should be a comment explaining the purpose of each line in your shell script.
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
