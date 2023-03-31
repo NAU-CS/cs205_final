728d764900dd391ffcc8bed83c228d5251ec8393
Jordan <jss576@nau.edu>
2023-03-31 07:11:58 +0000

    Script for Pokemon Data

---
+++ cs205_final_exam.sh
@@ -0,0 +1,40 @@
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file 
+#formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders:)
+# ===== SUMMARY OF DATA FILE =====
+#    File name: [VALUE]
+#    Total Pokemon: [VALUE]
+#    Avg. HP: [VALUE]
+#    Avg. Attack: [VALUE]
+# ===== END SUMMARY =====
+
+# The "Avg." values should be calculated as mean values for the corresponding columns.
+# The spacing and header formatting should match the above formatting description exactly.
+# There should be a comment explaining the purpose of each line in your shell script. 
+# The data file will be passed in to the script as a positional parameter and will not necessarily be 
+#called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+#Define file name variable
+file_name=$1
+
+#Print file name
+echo "File name:" $file_name
+
+#Number of Pokemon
+
+Pokemon_total=$(awk 'END{print (NR-1)}' $file_name)
+
+echo "Total Pokemon:" $Pokemon_total
+
+#Calculate average hp
+
+Average_HP=$(awk '{sum+=$6}END{print sum/$Pokemon_total}' $file_name)
+
+echo "Average HP:" $Average_HP
+
+#Calculate average attack
+
+Average_attack=$(awk '{sum+=$7}END{print sum/$Pokemon_total}' $file_name)
+
+echo "Avg. Attack:" $Average_attack
