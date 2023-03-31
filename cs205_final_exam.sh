ebad8cb511e8319adda25d1996966d3963edc525
DevinJay SanNicolas <devinjaysannicolas@gmail.com>
2023-03-31 16:28:32 -0700

    edited cs205_final_exam.sh and added screenshot of output its output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,4 +9,19 @@
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted
+
+FILENAME=$1
+
+totalPokemon=$(awk 'END {print NR-1}' ${FILENAME})
+
+avgHP=$(awk '{sum+=$6} END {print sum/NR}' ${FILENAME})
+
+avgAttack=$(awk '{sum+=$7} END {print sum/NR}' ${FILENAME})
+
+echo "===== SUMMARY OF DATA FILE ====="
+echo "    File name: ${FILENAME}"
+echo "    Total Pokemon: ${totalPokemon}"
+echo "    Avg. HP: ${avgHP}"
+echo "    Avg. Attack: ${avgAttack}"
+echo "===== END SUMMARY ====="
