b15c41afa7542520a667550c2f346e6aa3fa9c82
swm64 <swm64@nau.edu>
2023-03-31 19:16:02 -0700

    Edied cs205_final_exam.sh to complete TODO comments Added screenshots folder, containing screenshot of working script

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,27 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#print opening line of summary
+awk 'END {print " ===== SUMMARY OF DATA FILE ====="}' $1
+
+#print file name
+awk 'END {printf"    File name: "
+          print FILENAME }' $1
+          
+#print total number of rows - 1, to exclude header row
+awk 'END {printf "    Total Pokemon: " 
+          print (FNR-1)}' $1\
+          
+#print average of HP column
+awk '{totalHP += $6} 
+     END {printf "    Avg. HP: "
+          print (totalHP/(FNR-1))}' $1
+          
+#print average of attack column
+awk '{totalATK += $7} 
+     END {printf "    Avg. Attack: "
+          print (totalATK/(FNR-1))}' $1
+
+#print ending line
+awk 'END {print " ===== END SUMMARY ====="}' $1
