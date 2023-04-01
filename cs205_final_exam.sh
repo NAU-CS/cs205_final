18b80f7050302ce52da89d5fd72d37c50acce76a
Jarred D <jbeardunlap@gmail.com>
2023-03-31 21:17:08 -0700

    Edited script to function correctly. Loaded awk script into my virtualbox and matched up the sums with the appropriate columns.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -11,6 +11,9 @@
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
 
-#!/bin/bash
 
-{FS="\t"} {sum += $2} {sum1 += $3} {count++} END {print "File name: " FILENAME; print "Total Pokemon: " count; print "Avg. HP: " sum/count; print "Avg. Attack: " sum1/count} ' $1
+#I'm still unable to install git in virtual box. It says jj3571 (my username most places) is not in the list of sudoers. I could not find a way to fix this. I followed an online guide previously but it did not work, so instead I've been using VSCode. In an attempt to not cheat, I've also been using the terminal when possible to align with the intended Git usage. 
+
+#!/bin/awk -f
+
+awk 'BEGIN {FS="\t"} {count++}{sum+=$2}{sum1+=$6/NR}{sum2+=$7/NR} END {print "File Name: "FILENAME;print "Total pokemon: "count;print "Avg. HP: "sum1;print "Avg. Attack: "sum2}'$1 
