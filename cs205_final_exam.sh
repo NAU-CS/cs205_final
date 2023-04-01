c4b37ca58b927c1eb4c713bec47ea4b3dcf4b11d
hl596 <hl596@nau.edu>
2023-04-01 05:44:19 +0000

    This is a commit for changes to my .sh file for the final project

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-filename = $1
-total = $(awk 'END{print NR}' $filename)
-avg_hp = $(awk '{sum += $3} END {print sum / NR}' $filename)
-avg_attack = $(awk '{sum += $4} END {print sum / NR}' $filename=
+filename=$1
+total=$(awk 'END{print NR}' $filename)
+avg_hp=$(awk '{sum += $6} END {print sum / NR}' $filename)
+avg_attack=$(awk '{sum += $7} END {print sum / NR}' $filename)
 
-echo # ===== SUMMARY OF DATA FILE =====
-echo #    File name: [$filename]
-echo #    Total Pokemon: [$total]
-echo #    Avg. HP: [$avg_hp]
-echo #    Avg. Attack: [$avg_attack]
-echo # ===== END SUMMARY =====
+echo "# ===== SUMMARY OF DATA FILE ====="
+echo "#    File name: [$filename]"
+echo "#    Total Pokemon: [$total]"
+echo "#    Avg. HP: [$avg_hp]"
+echo "#    Avg. Attack: [$avg_attack]"
+echo "# ===== END SUMMARY ====="
 
