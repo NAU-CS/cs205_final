fe808a7d8e4b6209a7c9e9d8d08b9ddcd6d10d8f
Tyler <bryler2005@gmail.com>
2023-03-31 18:38:00 -0700

    added screenshot dir and made required changes to cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,85 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+#!/bin/bash
+
+#init variables
+filename=$1 # param 1 is stored as filename
+
+totalPokemon=0
+
+totalHP=0
+avgHP=0
+
+totalAttack=0
+avgAttack=0
+
+# calculate total number of pokemon
+totalPokemon=$(wc -l $filename | cut -d " " -f 1)
+
+totalPokemon=$(expr $totalPokemon - 1)
+
+
+
+# calculate average HP
+totalHP=$(tail +2 pokemon.dat | awk '{
+    if($5 ~ /[0-9]+/)
+    {
+         totalHP+=$5
+    }
+
+    else if($6 ~ /[0-9]+/)
+    {
+        totalHP+=$6
+    }
+    
+    else
+    {
+        totalHP+=$7
+    }
+} END{print totalHP}')
+
+avgHP=$( expr $totalHP / $totalPokemon )
+
+# calculate total Attack
+
+totalAttack=$(tail +2 pokemon.dat | awk '{
+    if($5 ~ /[0-9]+/)
+    {
+         totalAttack+=$6
+    }
+
+    else if($6 ~ /[0-9]+/)
+    {
+        totalAttack+=$7
+    }
+    
+    else
+    {
+        totalAttack+=$8
+    }
+} END{print totalAttack}')
+
+avgAttack=$( expr $totalAttack / $totalPokemon )
+
+# display results
+
+printf "===== SUMMARY OF DATA FILE =====\n"
+    # display file name
+printf "    File name: $filename\n"
+    # display total pokemon
+    
+printf "    Total Pokemon: $totalPokemon\n"
+
+    # display avg HP
+printf "    Avg. HP: $avgHP\n"
+    # display avg attack
+printf "    Avg. Attack: $avgAttack\n"
+
+
+printf "===== END SUMMARY =====\n"
+
+
+
+
+
