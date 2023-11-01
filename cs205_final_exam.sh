02e4b34e4ffe5a7f004bcc8758ad200d0d6980f5
Lucas Larson <lucaslarson25@gmail.com>
2023-11-01 13:47:16 -0700

    Edited cs205_final_exam.sh to print total number, average of HP, and average of Attack. /screenshots/screenshow.png is a screenshot of the script running.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,13 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+FILE=$1 #create a variable for the filename
+TOTAL=$(awk -F ' ' 'END{print $1}' $FILE) #find the last number in the first column with awk
+AVGHP=$(awk -F ' ' '{sum += $6}END{print sum/NR}' $FILE) #find the average of the HP column
+AVGATK=$(awk -F ' ' '{newsum += $7}END{print newsum/NR}' $FILE) #find the average of the attack column
+echo "===== SUMMARY OF DATA FILE =====" #print the header
+echo "   File name: $FILE" #print the filename
+echo "   Total Pokemon: $TOTAL" #print the total variable
+echo "   Avg. HP: $AVGHP" #print the AVGHP variable
+echo "   Avg. Attack: $AVGATK" #print the AVGATK variable
+echo "===== END SUMMARY =====" #print the closing message
