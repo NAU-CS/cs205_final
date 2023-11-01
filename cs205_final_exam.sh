0fe11bd19a09345426895f6e9fe1c76b1892de7b
cks276 <cks276.nau.edu>
2023-10-31 18:56:45 -0700

    -edit cs205 script to accept file input and process data and prints the result in a format that is human readable -created new subdirectory 'screenshots' and added screenshot to dir

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,17 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+#!/bin/bash
+
+# This line initializes variables to be used to collect data from file
+BEGIN {FS"\t"; total_pokemon=0; health_points=0; attack=0; FILE=$1}
+	# This body adds the sum of HP and attack
+	{health_points+=$6; attack+=$7}
+# This line processes data and displays result in the specified format
+END {print "===== SUMMARY OF DATA FILE =====";
+	print "   File name: " ARGV[1];
+	print "   Total Pokemon: " NR;
+	print "   Avg. HP: " health_points/NR;
+	print "   Avg. Attack: " attack/NR;
+	print "===== END SUMMARY ====="}
