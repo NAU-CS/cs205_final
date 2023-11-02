1075d3b15314f60143154dd157bef340e68fad45
Nick <nick@Ubuntu.myguest.virtualbox.org>
2023-11-01 17:36:40 -0700

    This is the final commit for the final exam

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -6,6 +6,23 @@
 #    Avg. Attack: [VALUE]
 # ===== END SUMMARY =====
 
+#$1 is the variable for the filename passed through the executable line. For example, if I run a command "$prog1.sh prog2.dat" $1 will have the value "prog2.dat" in it.
+FILE="$1"
+#To find the total pokemon, we cannot simply use the largest number in column 1 since there are repeated pokemon in the calculations for attack and hp, so we find the amount of newlines -1 to acount for the header.
+TOTAL=$(awk 'BEGIN {max = 0} NR > max {max = NR} END {print max-1}' $FILE)
+#To find the HP, we need to tell awk that the deliniator between columns is a tab ("\t"). Then we add up all the hp, which is column 5, and divide it by the total number of pokemon. 
+HP=$(awk 'BEGIN {FS = "\t"} {hp += $5} END {print hp/$TOTAL}' $FILE)
+#Similarly, to find average HP we note that the deliniator is a tab, add up all the attack, which is column 6, and then divide it by the total number of pokemon.
+ATK=$(awk 'BEGIN {FS = "\t"} {atk += $6} END {print atk/$TOTAL}' $FILE)
+
+#This echos the format exactly shown in the example.
+echo "==== SUMMARY OF DATA FILE ===="
+echo "    File name: $FILE"
+echo "    Total Pokemon: $TOTAL"
+echo "    Avg. HP: $HP"
+echo "    Avg. Attack: $ATK"
+echo "===== END SUMMARY ====="
+
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
