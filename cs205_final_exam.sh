adf48632dc886f82f109ccd4ce1746911e8ce940
jdm869@nau.edu <jdm869@nau.edu>
2023-11-02 20:37:22 -0700

    modified cs205_final_exam.sh and added screenshots

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,45 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#!/bin/bash
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+# Check if the user provided a file as a command line argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <data_file>"
+    exit 1
+fi
+
+# Get the filename from the command line argument
+data_file="$1"
+
+# Use awk to calculate the total, average HP, and average Attack values
+awk -F'\t' '
+    BEGIN {
+        # Initialize variables
+        total_pokemon = 0
+        total_hp = 0
+        total_attack = 0
+    }
+    NR > 1 {
+        # Skip the header line
+        total_pokemon++
+        total_hp += $6  # HP column
+        total_attack += $7  # Attack column
+    }
+    END {
+        if (total_pokemon > 0) {
+            # Calculate the averages
+            avg_hp = total_hp / total_pokemon
+            avg_attack = total_attack / total_pokemon
+        } else {
+            avg_hp = 0
+            avg_attack = 0
+        }
+
+        # Print the summary report
+        printf("===== SUMMARY OF DATA FILE =====\n")
+        printf("   File name: %s\n", ARGV[1])
+        printf("   Total Pokemon: %d\n", total_pokemon)
+        printf("   Avg. HP: %.2f\n", avg_hp)
+        printf("   Avg. Attack: %.2f\n", avg_attack)
+        printf("===== END SUMMARY =====\n")
+    }
+' "$data_file"
+
