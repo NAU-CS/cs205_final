db6bb0778bdf665c0440422e35ef73c920da9a8c
dylan <dmg547@nau.edu>
2023-11-02 22:23:50 -0700

    updated cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -18,5 +18,3 @@ END{print "   Total Pokemon: "NR}
 {healthsum += $6} END{print "   Avg. HP: "healthsum/NR}
 {attacksum += $7} END{print "   Avg. HP: "attacksum/NR}
 END{print "===== END SUMMARY ====="}
-NR)
-{print "===== END SUMMARY ====="}
