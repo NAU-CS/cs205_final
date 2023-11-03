669de0bfbb32a3fe59471fec1502932eabd15027
Jakegams808 <149110648+Jakegams808@users.noreply.github.com>
2023-11-02 20:25:15 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,7 +10,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
-print "File name:"
-print "Total Pokemon:"
-print "Avg. HP:"
+FILE="$1"
+LINE= wc -l <$FILE
+AVGHP= sort -k 6 $FILE >sum
+AVGAT= sort -k 7 $FILE >sum
+
+echo " ===== SUMMARY OF DATA FILE ====="
+echo "File name:" $FILE
+echo "Total Pokemon:" $LINE
+echo "Avg. HP: " $AVGHP
+echo "Avg. Attack: " $AVGAT
+echo " ===== END SUMMARY ====="rint "Avg. HP:"
 print "Avg. Attack:"
