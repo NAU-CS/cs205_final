0fb2d4bdab3480ef4ef936166051725c53a48304
dylan <dmg547@nau.edu>
2023-11-02 22:28:00 -0700

    updated cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -11,10 +11,20 @@
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
 
-
+# added a header of data file start summary
 END{print "===== SUMMARY OF DATA FILE ====="}
+
+# added function to print file name
 END{print "   File name: "FILENAME}
+
+# added function to print total pokemon, which is just number of lines
 END{print "   Total Pokemon: "NR}
+
+# added function to add the total number of values in column 6, then divide that total by the pokemon total
 {healthsum += $6} END{print "   Avg. HP: "healthsum/NR}
+
+# added funciton to add the total number of values in column 7, then divide that total by the pokemon total
 {attacksum += $7} END{print "   Avg. HP: "attacksum/NR}
+
+# added a footer of data file end summary
 END{print "===== END SUMMARY ====="}
