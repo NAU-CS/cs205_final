d0b73cce7cd9a2241a2a0bdfb80979cbfc2b74e0
dylan <dmg547@nau.edu>
2023-11-02 22:16:28 -0700

    updated cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,11 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+
+END{print "===== SUMMARY OF DATA FILE ====="}
+END{print "   File name: "FILENAME}
+END{print "   Total Pokemon: "NR}
+{healthsum += $6} END{print "   Avg. HP: "healthsum/NR}
+{attacksum += $7} END{print "   Avg. HP: "attacksum/NR}
+END{print "===== END SUMMARY ====="}
