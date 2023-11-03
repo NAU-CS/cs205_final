6d33e3ef1bc356c1af7f7ea43ee6ecf3619ee2c3
gabby <gmd264.nau.edu>
2023-11-03 14:47:15 -0700

    adding the script to list the file name, total pokemon, avg. hp, and avg. attack

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,32 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <data_file>"
+	exit 1
+fi
+
+input_file="$1"
+
+awk 'BEGIN {
+	total_hp = 0
+	total_attack = 0
+	total_pokemon = 0
+}
+
+{
+	total_hp += $5
+	total_attack += $6
+	total_pokemon++
+}
+
+END {
+	avg_hp = total_hp / total_pokemon
+	avg_attack = total_attack / total_pokemon
+
+	printf("File name: %s\n", FILENAME)
+	printf("Total Pokemon: %d\n", total_pokemon)
+	printf("Avg. HP: %2f\n", avg_hp)
+	printf("Avg. Attack: %.2f\n", avg_attack)
+}' "$input_file"
