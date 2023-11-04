7a17809559eeb5d0e70ce909137863021d829a0c
George <gmv59@nau.edu>
2023-11-03 18:10:46 -0700

    Modify cs205_final_exam.sh and add screenshot

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,22 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+if [$# -ne 1 ]; then
+	echo "Usage: $0 best_pokemon.dat"
+exit 1
+fi
+
+awk 'BEGIN {
+    FS="\t";  
+    print "Stat\tMin\tMax\tAvg.";
+}
+{
+    total += $2; 
+    count++;
+}
+END {
+    avg_attack = total / count;
+    print "Attack\t-999\t999\t" avg_attack;
+}' "$1"
+
+exit 0
