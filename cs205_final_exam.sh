65725549bd61ad9fbbb299992930fa5b1cf746f8
Jake <jh3837@nau.edu>
2023-11-03 21:40:07 -0700

    Commits

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -5,12 +5,13 @@ fi
 
 file_name="$1"
 
-avg_hp=$(awk '{ sum += $2 } END { if (NR > 0) print sum / NR }' "$file_name")
-avg_attack=$(awk '{ sum += $3 } END { if (NR > 0) print sum / NR }' "$file_name")
+avg_hp=$(awk '{ sum += $5 } END { if (NR > 0) print sum / NR }' "$file_name")
+avg_attack=$(awk '{ sum += $6 } END { if (NR > 0) print sum / NR }' "$file_name")
+total_pokemon=$(awk 'NR > 1 { count++ } END { print count }' "$file_name")
 
 echo "==== SUMMARY OF DATA FILE ===="
 echo "File name: $file_name"
-echo "Total Pokemon: $(wc - 1 < "$file_name")"
+echo "Total Pokemon: $total_pokemon"
 echo "Avg. HP: $avg_hp"
 echo "AVG. Attack: $avg_attack"
 echo "==== END SUMMARY ===="
