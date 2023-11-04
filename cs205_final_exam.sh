4473171c0f172aeefadd6b319b732e48f4ebc1a0
spj47 <spj47@nau.edu>
2023-11-03 21:09:20 -0600

    Added updated cs205_final_exam.sh and screenshots

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,11 +1,35 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ======= SUMMARY OF POKEMON.DAT ======
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ======= END SUMMARY =======
+#!/bin/bash
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+if [ $# -ne 1 ]; then
+     echo "Please use a file"
+     exit 1
+fi
+
+file="$1"
+
+summary=$(awk -F'\t' 'BEGIN {
+     total_pokemon = 0
+     total_hp=0
+     total_attack = 0
+}
+
+NR > 1 { 
+     total_pokemon++
+     total_hp+=$6
+     total_attack += $7
+}
+
+END { 
+     avg_hp = total_hp / total_pokemon
+     avg_attack = total_attack / total_pokemon
+
+     printf("===== SUMMARY OF DATA FILE =====\n")
+     printf("    File name: %s\n", ARGV[1])
+     printf("    Total Pokemon: %d\n", total_pokemon)
+     printf("    Avg. HP: %.2f\n", avg_hp)
+     printf("    Avf. Attack: %.2f\n", avg_attack)
+     printf("===== END SUMMARY =====\n")
+}' "$file")
+
+echo "$summary"
+exit 0
