a653705e9cacd133a0abfa0b1655e2b8a7e3bf84
Kalpek <crazychickennn9@gmail.com>
2023-11-03 17:49:54 -0700

    I created the screenshots folder to show the screenshot of my code calculating the average hp, filename of the data file I was calculating from,total number of pokemon, and average attack of the pokemon in the data file. I also modified the cs205_final_exam.sh file to have the source code for those calculations.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,18 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#once this file is being executed after the command, it makes sure a file was provided and stores it into the variable file_name for later use.
+data_file="$1"
+file_name=$(basename "$data_file")
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+#since hp is in the 4th column, i create a variable called sum_hp in order to add each hp value to that sum using $4 to get the value from the 4th column of each line. then, i divide that by the NR variable, which counts the total number of lines that have been crossed over so far. This gives me the average hp for each pokemon. 
+avg_hp=$(awk 'BEGIN { sum_hp = 0 } {sum_hp += $4} END {print sum_hp / NR}' "$data_file")
+#I repeat the same exact process for attack, but instead of targetting column 4, we use column 5 since column 5 is what holds each pokemons attacking stat. Same thing, divide by NR that counts all the lines, and since we are doing this concurrently with HP it'll save computing power rather than iterating through everything twice. 
+avg_attack=$(awk 'BEGIN { sum_atk = 0 } {sum_atk += $5} END {print sum_atk / NR}' "$data_file")
+#to get the total pokemon, instead of iterating again after getting the averages, we can just print NR since that already holds the value for how many lines we have iterated over during the process we used to get the averages.
+total_pokemon=$(awk 'END { print NR }' "$data_file")
+
+#then just print the summary as instructed. basically just prints the variables we calculated above.
+echo "===== SUMMARY OF DATA FILE ====="
+echo "File name: $file_name"
+echo "Total Pokemon: $total_pokemon)"
+echo "Avg. HP: $avg_hp"
+echo "Avg. Attack: $avg_attack"
+echo "===== END SUMMARY ====="
