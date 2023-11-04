ec0d0c3e0be2b9757c8e3b246dc5bff90b5e4d88
Yale <yhc4@nau.edu>
2023-11-03 20:45:44 -0600

    commit made to cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,33 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+filename="$1"
+
+awk '
+	BEGIN {
+		total_pokemon = 0
+		total_hp = 0
+		total_attack = 0
+	}
+	{
+		total_pokemon++
+		total_hp += $2
+		total_attack += $3
+	}
+	
+	END {
+		if (total_pokemon > 0) {
+			avg_hp = total_hp / total_pokemon
+			avg_attack = total_attack / total_pokemon
+		} else {
+			avg_hp = 0
+			avg_attack = 0
+		}
+		
+		print "File name: " FILENAME
+		print "Total Pokemon: " total_pokemon
+		printf "Avg. HP: %.2f\n", avg_hp
+		printf "Avg. Attack: %.2f\n", avg_attack
+	}
+' "$filename"
