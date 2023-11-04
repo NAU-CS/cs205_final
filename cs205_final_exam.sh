7ebc653d35639f94a2948511cd6c684c7c528b13
Jaelenn <jaelenn.jj@gmail.com>
2023-11-04 01:30:24 -0700

    Your commit message

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -4,48 +4,36 @@
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
 # The data file will be passed into the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
 
+
 #!/bin/bash
 
-# Check to see if there was exactly 1 argument entered
+# Check if a file argument is provided
 if [ "$#" -ne 1 ]; then
-    # If more or less than 1 argument was presented, print this message
     echo "$0 requires 1 file as an argument."
-    # and exit the script
     exit 1
 fi
 
-# Use the filename entered in the argument
+# Assign the file name from the argument
 file_name="$1"
 
-# create summary, use awk to calculate values
-summary=$(
-    # let awk know it is separated by indentations
-    awk -F '\t' '
-    # skip the header line
-    NR == 1 {
-        # Go to the next line
-        next
-    }
-    {
-        # count the number of Pokemon
-        total++
-        # sum up the hp values
-        hp += $6
-        # sum up the attack values
-        attack += $7
-    }
-    END {
-        # calculate average hp
-        avg_hp = hp / total
-        # calculate average attack
-        avg_attack = attack / total
-        # print the results
-        printf "Avg. HP: %.2f\n", avg_hp
-        printf "Avg. Attack: %.2f\n", avg_attack
-    }
-' "$file_name" # don't forget to pass the file name to awk
-) # close the command here
+# Print the summary header
+echo "====SUMMARY OF DATA FILE ===="
+echo "File name: $file_name"
 
-# print the summary
-echo "$summary"
+# Find the total number of lines
+total_pokemon=$(awk 'NR > 1 { count++ } END { print count }' "$file_name")
+echo "Total Pokemon: $total_pokemon"
+
+# Calculate the average HP
+avg_hp=$(awk 'NR > 1 { sum += $5 } END { if (NR > 1) printf "%.2f", sum / (NR - 1) }' "$file_name")
+echo "Avg. HP: $avg_hp"
+
+# Calculate the average Attack
+avg_attack=$(awk 'NR > 1 { sum += $6 } END { if (NR > 1) printf "%.2f", sum / (NR - 1) }' "$file_name")
+echo "Avg. Attack: $avg_attack"
+
+# Print the summary footer
+echo "==== END SUMMARY ===="
+
+
 
