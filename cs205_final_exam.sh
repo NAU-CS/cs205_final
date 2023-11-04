82cbb5badb5e31563300c60cf2271b5a4021ab0c
logan <logan@logan-VirtualBox>
2023-11-03 17:56:57 -0700

    final

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -14,11 +14,12 @@
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
 
 printf "===== SUMMARY OF DATA FILE =====\n"
-printf "   File name: \n"
-printf "   Total Pokemon: \n"
-printf "   Avg. HP: \n"
-printf "   Avg. Attack: \n"
-printf "===== END SUMMARY =====\n"
-
+printf "   File name: $1\n"
+printf "   Total Pokemon: "
+awk 'BEGIN {FS = "\t"}; {line += $2} {count++} END {print count}' best_pokemon.dat
+printf "   Avg. HP: "
 awk 'BEGIN {FS = "\t"}; {line += $6} {count++} END {print line / count}' best_pokemon.dat
+printf "   Avg. Attack: "
+awk 'BEGIN {FS = "\t"}; {line += $7} {count++} END {print line / count}' best_pokemon.dat
+printf "===== END SUMMARY ====="
 
