d200d312a80bea87a98c31637937e96d1a694675
mpa92 <128656183+mpa92@users.noreply.github.com>
2024-05-06 09:27:05 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,5 +1,43 @@
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
+
+# Check if the user has provided a file as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <data file>"
+    exit 1
+fi
+
+# Assign the file name to a variable
+filename=$1
+
+# Use awk to process the file, summarizing Pokémon data
+awk -v file="$filename" -F '\t' 'BEGIN {
+    total_hp = 0;
+    total_attack = 0;
+    count = 0;
+}
+
+# Skip the header and accumulate data from data lines
+NR > 1 {
+    total_hp += $5;       # Sum HP values from the 5th column
+    total_attack += $6;   # Sum Attack values from the 6th column
+    count++;              # Increment the counter for each line processed
+}
+
+END {
+    # Calculate average values
+    avg_hp = total_hp / count;
+    avg_attack = total_attack / count;
+    
+    # Print the summary including the file name
+    print "===== SUMMARY OF DATA FILE =====";
+    print "   File name: " file;
+    print "   Total Pokemon: " count;
+    printf "   Avg. HP: %.2f\n", avg_hp;
+    printf "   Avg. Attack: %.2f\n", avg_attack;
+    print "===== END SUMMARY =====";
+}' "$filename"
+
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
 #    Avg. HP: [VALUE]
