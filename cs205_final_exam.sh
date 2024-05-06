17f3a84e59abc3209276e3f5cc487229ba8c1eeb
shreyadikshitt <165846908+shreyadikshitt@users.noreply.github.com>
2024-05-06 09:20:54 -0700

    Update cs205_final_exam.sh
    modified script according to instructions

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,26 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+#!/bin/bash
+
+# Set the file name based on the positional parameter
+file_name=$1
+
+# Print the header of the report
+echo "===== SUMMARY OF DATA FILE ====="
+echo "   File name: $file_name"
+
+# Count the total number of Pokemon (excluding the header)
+total_pokemon=$(awk 'NR>1 {count++} END {print count}' "$file_name")
+echo "   Total Pokemon: $total_pokemon"
+
+# Calculate the average HP
+avg_hp=$(awk 'NR>1 {total_hp += $2} END {print total_hp/NR}' "$file_name")
+echo "   Avg. HP: $avg_hp"
+
+# Calculate the average Attack
+avg_attack=$(awk 'NR>1 {total_attack += $3} END {print total_attack/NR}' "$file_name")
+echo "   Avg. Attack: $avg_attack"
+
+# Print the footer of the report
+echo "===== END SUMMARY ====="
