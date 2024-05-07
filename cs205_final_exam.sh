6b36c2df3ce923e5a9b5773f3f7754ab919c6302
Tyson <tjc558@nau.edu>
2024-05-06 20:33:41 -0700

    commit cs205 final

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,5 +1,31 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# TODO: Modify this file to create a shell script
+# that is able to use awk to go through a file
+# formatted like best_pokemon.dat and provides a printed
+# report in the following format
+# (where your script correctly calculates the values
+# that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
+BEGIN{
+
+total = 0;
+avg_attack = 0;
+avg_hp = 0;
+FS = "\t";
+}
+{
+if(NR>1){
+avg_attack += $7;
+avg_hp += $6;
+
+total += 1;
+}
+}
+END{
+print "File name: " FILENAME;
+print "Total Pokemon: " total; 
+print "Avg. Attack: " avg_attack/(total);
+print "Avg. HP: " avg_hp/(total);
+} 
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
 #    Avg. HP: [VALUE]
@@ -9,4 +35,6 @@
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+# The data file will be passed in to the script as a positional parameter and will not necessarily be
+# called best_pokemon.dat. However, you can assume that any file passed to this script
+# will be formatted exactly the way best_pokemon.dat is formatted.
