79709b6b8f8c176886aa0049c2d7c6d775b4616c
Mitchell Morris <mitchellmorris@Mitchells-MacBook-Pro-3.local>
2024-05-06 18:11:08 -0700

    adding and commit sh file

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,9 +9,7 @@
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted
-
-
+# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
 FILE=$1
 # prints the file name
 echo "File name: $FILE"
