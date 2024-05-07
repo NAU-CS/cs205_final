2c091cb198e0158f93e62ee217f7383e091ca9e2
JohnrKing <149135626+JohnrKing@users.noreply.github.com>
2024-05-06 22:30:39 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,10 +1,15 @@
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+filename=$1
+total=$(awk 'END {print NR}' $filename)
+avg_hp=$(awk '{sum += $3} END {print sum / NR}' $filename)
+avg_attack=$(awk '{sum += $4} END {print sum / NR}' $filename)
+echo "===== SUMMARY OF DATA FILE ====="
+echo "filename: $filename"
+echo "Total Pokemon: $total"
+echo "Avg. HP: $avg_hp"
+echo "Avg. Attack: $avg_attack"
+echo "===== END SUMMARY ====="
+
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
