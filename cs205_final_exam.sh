84df493647335781f5b322b694577ccb2fae9180
Maddox Ray <maddox@Maddoxs-MacBook-Air.local>
2024-05-07 22:05:42 -0700

    Added the script require to analyze pokemon data, created a new file where the output of this script is store via screenshot.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,30 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#!/bin/bash
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+# Purpose: Check if a file is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <data_file>"
+    exit 1
+fi
+
+# Purpose: Store the file name passed as argument
+file_name="$1"
+
+# Purpose: Print the header for the summary
+echo "===== SUMMARY OF DATA FILE ====="
+echo -n "   File name: "
+echo "[${file_name}]"
+
+# Purpose: Calculate and print total number of Pokemon
+total_pokemon=$(awk 'END {print NR}' "$file_name")
+echo "   Total Pokemon: ${total_pokemon}"
+
+# Purpose: Calculate and print average HP
+avg_hp=$(awk '{sum += $5} END {print sum/NR}' "$file_name")
+echo "   Avg. HP: ${avg_hp}"
+
+# Purpose: Calculate and print average Attack
+avg_attack=$(awk '{sum += $4} END {print sum/NR}' "$file_name")
+echo "   Avg. Attack: ${avg_attack}"
+
+# Purpose: Print end of summary
+echo "===== END SUMMARY ====="
