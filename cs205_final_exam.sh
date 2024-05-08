561d6a89bed2dda28084b3b1b48c768ab5315097
Hunter M Beach <hmb433@nau.edu>
2024-05-08 02:33:48 -0700

    changed the SH files to specifcations, added screenshots folder, added screenshot of output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,10 +1,35 @@
+#!/bin/bash
+
+# Read first positional paramete into a varible
+input="$1"
+
+#Get The count of rows
+#PLAN: Use AWK, use NR to get row count, -1 for the header
+pokecount=$( awk 'END{print (NR-1)}' "$input")
+
+#Get Avg health
+#PLAN: access row 6 sum it, devide by poke count, skip header
+pokehealth=$( awk -F '\t' ' NR > 1 {sumhp += $6} END {print sumhp/'$pokecount'}' "$input")
+
+#Get AVG atk
+# Same as before, focus row 7
+#PLAN: access row 6 sum it, devide by poke count, skip header
+pokeatk=$( awk -F '\t' ' NR > 1 {sumatk += $7} END {print sumatk/'$pokecount'}' "$input")
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+
+# ehco title
+echo "===== SUMMARY OF DATA FILE ====="
+# Echo the file name
+echo "    File name: $input"
+# Echo the count
+echo "    Total Pokemon: $pokecount"
+# Echo the HP
+echo "    Avg. HP: $pokehealth"
+# Echo the avg attack
+echo "    Avg. Attack: $pokeatk"
+#echo summary
+echo "===== END SUMMARY ====="
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
