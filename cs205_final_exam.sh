72065f8a4bfbc6f08b7deee944e08ccc74f7c180
Kyle Draper <kpd87@nau.edu>
2024-05-08 07:02:42 -0700

    I modified the final script to give it code for the best_pokemon.txt file, by letting it take 1) a command line argument 2) count number of pokemon 3) take two averages, and then print out those results. In the screenshots folder, You will see a screenshot of the command running and its output.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,17 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+#
+
+FILENAME="$1"
+TotalPokemon=$(awk 'END{print $1}' $FILENAME)
+AVGHP=$(awk  'BEGIN{FS="\t"} { sum += $6; n++} END{ if (n > 0) print sum/n; }' $FILENAME )
+AVGATK=$(awk 'BEGIN{FS="\t"} { sum += $7; n++} END{ if (n > 0) print sum/n; }' $FILENAME )
+
+echo "===== SUMMARY OF DATA FILE ===="
+echo -e ' \tFile name:' $FILENAME
+echo -e ' \tTotal Pokemon:' $TotalPokemon
+echo -e ' \tAVG. HP:' $AVGHP
+echo -e ' \tAVG. Attack:' $AVGATK
+echo "===== END SUMMARY ====="
+
